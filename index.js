20d0040e9440e4bb33e02a76323c6178ab3e8142
Philip <philip_angelin@hotmail.com>
2020-05-09 11:16:45 +0200

    Method chaining and removed moment dependency
    Also minor fix if expiration parameter was sent in as null

--- index.js
+++ index.js
@@ -1,32 +1,26 @@
 class CookieCutter {
 	delete(key) {
 		document.cookie = `${key}=; expires=${new Date(0).toUTCString()}; path=/`;
-		return `${key}=; expires=${new Date(0).toUTCString()}; path=/`;
+		return this;
 	}
 
 	set(key, value, expiration = null, path = 'path=/') {
 		let cookie = [`${key}=${value}`];
 		if (expiration === null) {
-			let date = moment().add('days', 7);
-			expiration = date.toDate();
-		} else if (expiration) {
-			cookie.push('expires=' + expiration);
+			expiration = new Date(Date.now() + 604800000);
+            cookie.push('expires=' + expiration);
 		}
 		cookie.push(path);
 		cookie = cookie.join('; ');
 		document.cookie = cookie;
 		
-		return cookie;
+		return this;
 	}
 
 	get(key) {
 		let regex = new RegExp(`${key}=([^;]*);`, 'i');
 		let match = document.cookie.match(regex);
-		if (match !== null) {
-			return match[1];
-		} else {
-			return false;
-		}
+		return match !== null ? match[1] : false;
 	}
 }
 
